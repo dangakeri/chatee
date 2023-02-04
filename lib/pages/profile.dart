72f6a70fdb13d5e7afd3033b4ff07302df92056d
MainahMwangi <mainahmwangi12@gmail.com>
2023-02-03 19:15:04 -0800

    customAppBar

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/header.dart';
+
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: header(titleText: 'Profile'),
+    );
   }
 }
--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/header.dart';
+
 class Timeline extends StatelessWidget {
   const Timeline({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: header(titleText: ''),
+    );
   }
 }
